9ca2bfbf0758ec5260c65ee08214eb29392217e2
home <home@Chuloos-Macbook-Pro.local>
2019-07-22 03:24:42 +0100

    infinite scroll done

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,20 +3,32 @@ import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
 import "./gallery.css"
 
-const ImageGallery = ({ images, loading }) => {
+const ImageGallery = ({ images, loading, fetchImages }) => {
   // Create gallery here
   return (
-    <div className="image-grid">
-      {!loading
-        ? images.map((image, index) => (
-            <div className="image-item" key={index}>
-              <img src={image.urls.regular} />
-            </div>
-          ))
-        : "🤤🤤..."}
-    </div>
+    <InfiniteScroll
+      dataLength={images.length}
+      next={fetchImages(5)}
+      hasMore={true}
+      loader={
+        <p style={{ textAlign: "center", marginTop: "1%" }}>
+          More doggo incoming 🐕 🐕...
+        </p>
+      }
+    >
+      <div className="image-grid">
+        {!loading
+          ? images.map((image, index) => (
+              <div className="image-item" key={index}>
+                <img src={image.urls.regular} />
+              </div>
+            ))
+          : ""}
+      </div>
+    </InfiniteScroll>
   )
 }
 
@@ -31,17 +43,20 @@ const Page = () => {
   }, [])
 
   // Fetch Images
-  const fetchImages = count => {
+  const fetchImages = (count = 10) => {
     const apiRoot = "https://api.unsplash.com"
     const accessKey =
-      "1bf0ee2ba4572a36e6c0bb2ce87ab3f6bc7510fd8676791a0be3d7dbfd2c0acd"
+      // "a5a0aed7b2bc3fba7e79cdd18b934925ba51540a80a4aefd7d8e851d73235a22"
+      // "1bf0ee2ba4572a36e6c0bb2ce87ab3f6bc7510fd8676791a0be3d7dbfd2c0acd"
+      "8a4eb76e94d02735696730edac2a8e0fc8f6e4d8e6064fd4add3006183e6e372"
 
-    axios
-      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${10}`)
-      .then(res => {
-        setImages(res.data)
-        setLoading(false)
-      })
+    const dogEndpoint = `${apiRoot}/photos/random?client_id=${accessKey}&count=${count}&collections='3816141,1154337,1254279'`
+    const normalEndpoint = `${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`
+
+    axios.get(dogEndpoint).then(res => {
+      setImages([...images, ...res.data])
+      setLoading(false)
+    })
   }
 
   return (
@@ -49,10 +64,14 @@ const Page = () => {
       <SEO title="Gallery" />
       <h1 className="is-size-3">Images from Unsplash...</h1>
       <p style={{ marginBottom: "5%" }}>
-        Now this is the Law of the Jungle, as old and true as the sky... bleh
-        bleh bleh
+        Now this is the Law of the Jungle, as old and true as the sky, for as
+        long as you keep scrolling, you shall find more doggo images 🐶 🐕.
       </p>
-      <ImageGallery images={images} loading={loading} />
+      <ImageGallery
+        images={images}
+        loading={loading}
+        fetchImages={fetchImages}
+      />
     </Layout>
   )
 }
